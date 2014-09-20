c35bc1c0c3d790eff026c5fddeeeb290ce1dda26
Evan Jones <ej@evanjones.ca>
2014-09-20 18:49:26 -0400

    grep/fuzzy.go: Add optimized byte fuzzy matching
    This is rediculously way over-optimized, but if was fun.

--- grep/fuzzy.go
+++ grep/fuzzy.go
@@ -5,6 +5,52 @@ import (
 	"unicode/utf8"
 )
 
+/* BRANCHLESS MAGIC: nearly 2X faster than the naive approach
+
+HOW IT WORKS:
+
+The first term is negative if c >= A
+The second term is negative if c <= Z
+ANDing them together: negative only if both are negative
+SHIFT/AND: equals 32 if both terms are negative, zero otherwise
+
+The shift can be anywhere from 26 (max) to 2
+*/
+
+func asciiToLower(b byte) byte {
+	c := uint32(b)
+	c += (((('A' - 1) - c) & (c - ('Z' + 1))) >> 26) & 32
+	return byte(c)
+}
+
+// Returns true if s contains all the bytes in query in order, with any number of additional bytes
+// between them. It matches ASCII in a case-insensitive way.
+func containsBytesFuzzyInsensitive(s string, query string) bool {
+	// this is ridiculously over-optimized
+	// We unconditionally set the magicLowerBit for an approximate case-insensitive comparison
+	// if the are equal, we actually execute the significantly slower asciiToLower()
+	// this is about ~25% faster than always calling asciiToLower
+	const magicLowerBit = 0x20
+	qIndex := 0
+	qByteMaybeLower := query[qIndex] | magicLowerBit
+	qByteLower := asciiToLower(query[qIndex])
+	for i := 0; i < len(s); i++ {
+		sByteMaybeLower := s[i] | magicLowerBit
+		if sByteMaybeLower == qByteMaybeLower {
+			// now ACTUALLY check lowercase
+			if asciiToLower(s[i]) == qByteLower {
+				qIndex += 1
+				if qIndex == len(query) {
+					return true
+				}
+				qByteMaybeLower = query[qIndex] | magicLowerBit
+				qByteLower = asciiToLower(query[qIndex])
+			}
+		}
+	}
+	return false
+}
+
 func toRunes(s string) []rune {
 	if !utf8.ValidString(s) {
 		panic("invalid utf8 input")
@@ -27,6 +73,8 @@ func isWordEnd(previous rune, next rune) bool {
 	return false
 }
 
+// Returns a ranking of how well query matches path. Values less than zero do not match at all.
+// General order: Filename prefix match (case insensitive)
 func FuzzyMatch(query string, path string) int {
 	qRunes := toRunes(query)
 	pRunes := toRunes(path)
--- grep/fuzzy_test.go
+++ grep/fuzzy_test.go
@@ -1,6 +1,7 @@
 package grep
 
 import (
+	"strings"
 	"testing"
 )
 
@@ -25,5 +26,134 @@ func TestFuzzyMatch(t *testing.T) {
 	}
 	if fourth < 0 {
 		t.Error("last must match:", fourth)
+	}
+}
+
+func TestContainsBytesFuzzy(t *testing.T) {
+	const query = "abcde"
+
+	bad := []string{
+		"",
+		"abcd",
+		"----a-b-c-d----",
+		"------",
+		"Abcde",
+	}
+	good := []string{
+		"abcde",
+		"-a-b-c-d-e-",
+	}
+	for _, s := range bad {
+		if containsBytesFuzzy(s, query) {
+			t.Error(s + " must not contain " + query + " (returned true)")
+		}
+	}
+	for _, s := range good {
+		if !containsBytesFuzzy(s, query) {
+			t.Error(s + " must contain " + query + " (returned false)")
+		}
+	}
+}
+
+func TestContainsBytesFuzzyInsensitive(t *testing.T) {
+	const query = "abcde"
+
+	bad := []string{
+		// TODO: accent folding? unicode to lower?
+		"abcdé",
+	}
+	good := []string{
+		"ABCDE",
+		"-A-B-C-D-E-",
+	}
+	for _, s := range bad {
+		if containsBytesFuzzyInsensitive(s, query) {
+			t.Error(s + " must not contain " + query + " (returned true)")
+		}
+	}
+	for _, s := range good {
+		if !containsBytesFuzzyInsensitive(s, query) {
+			t.Error(s + " must contain " + query + " (returned false)")
+		}
+	}
+}
+
+func TestToLower(t *testing.T) {
+	if asciiToLower('@') != '@' {
+		t.Error("error")
+	}
+	if asciiToLower('A') != 'a' {
+		t.Error("error")
+	}
+	if asciiToLower('Z') != 'z' {
+		t.Error("error")
+	}
+	if asciiToLower('[') != '[' {
+		t.Error("error")
+	}
+	for i := 0; i < 256; i++ {
+		if asciiToLower(byte(i)) != asciiToLowerSlow(byte(i)) {
+			t.Error("Wrong output for byte:", i)
+		}
+	}
+}
+
+const query = "abcde"
+const s = "01234567890123456789a01234567890123456789b01234567890123456789c01234567890123456789d01234567890123456789e"
+
+func containsBytesFuzzy(s string, query string) bool {
+	qIndex := 0
+	for i := 0; i < len(s); i++ {
+		if s[i] == query[qIndex] {
+			qIndex += 1
+			if qIndex == len(query) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func BenchmarkContainsBytes(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		containsBytesFuzzy(s, query)
+	}
+}
+
+func containsBytesFuzzyInsensitiveToLower(s string, query string) bool {
+	sLower := strings.ToLower(s)
+	qLower := strings.ToLower(query)
+	return containsBytesFuzzy(sLower, qLower)
+}
+
+func BenchmarkContainsBytesInsensitiveToLower(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		containsBytesFuzzyInsensitiveToLower(s, query)
+	}
+}
+
+func BenchmarkContainsBytesInsensitive(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		containsBytesFuzzyInsensitive(s, query)
+	}
+}
+
+func BenchmarkAsciiToLower(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		asciiToLower('a')
+	}
+}
+
+func asciiToLowerSlow(b byte) byte {
+	const diff = 'a' - 'A'
+	if 'A' <= b && b <= 'Z' {
+		b += diff
+	}
+	return b
+}
+
+func BenchmarkAsciiToLowerSlow(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		asciiToLowerSlow('a')
 	}
 }
